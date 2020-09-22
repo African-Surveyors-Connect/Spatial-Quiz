8f5fd29fd902f7b361105f6e83e7806634978c02
surveyorjr <matingonk@gmail.com>
2020-09-22 19:43:14 +0200

    initial commit from clone

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://localhost/savsoftquiz_v4.0_advance/';
+		$sq_hostname='localhost';
+		$sq_dbname='savsoft_quiz_v4';
+		$sq_dbusername='root';
+		$sq_dbpassword='';
+		?>
